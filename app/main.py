79cb65aab34de828604ffb9937e7d49256c463f7
jonfernandez12 <jonferuam@gmail.com>
2022-06-21 23:04:32 +0200

    test: testing main ,repository and validator

--- app/main.py
+++ app/main.py
@@ -11,18 +11,8 @@ Application for splitting concatenated street names and numbers into
 formatted json objects using different processors.
 """
 
-if __name__ == "__main__":
-    processed_data = []
-    logger.info("Service started with version {}".format(__version__))
-    logger.info("Creating data processor, please wait...")
-    try:
-        processor = Processor()
-        logger.info("\u2728  Success creating data processor!  \u2728")
-    except Exception as e:
-        logger.error(
-            "\u274C Error creating data processor, " + str(e) + "\u274C"
-        )
 
+def init_validator() -> Validator:
     logger.info("Creating data validator, please wait...")
     try:
         validator = Validator()
@@ -31,7 +21,22 @@ if __name__ == "__main__":
         logger.error(
             "\u274C Error creating data validator, " + str(e) + "\u274C"
         )
+    return validator
 
+
+def init_processor() -> Processor:
+    logger.info("Creating data processor, please wait...")
+    try:
+        processor = Processor()
+        logger.info("\u2728  Success creating data processor!  \u2728")
+    except Exception as e:
+        logger.error(
+            "\u274C Error creating data processor, " + str(e) + "\u274C"
+        )
+    return processor
+
+
+def init_repository() -> Repository:
     logger.info("Accessing data with the repository, please wait...")
     try:
         repository = Repository()
@@ -42,56 +47,81 @@ if __name__ == "__main__":
             + str(e)
             + "\u274C"
         )
+    return repository
+
+
+def get_public_methods_from_processor(processor: Processor) -> list:
+    public_method_names = [
+        method
+        for method in dir(processor)
+        if callable(getattr(processor, method))
+        if not method.startswith("_")
+    ]
+    return public_method_names
+
+
+def call_all_processors(
+    public_method_names: list, processor: Processor, repository: Repository
+) -> list:
+    processed_data = []
+    for method in public_method_names:
+        processed_data.append(
+            {
+                "method": method,
+                "processed_data": getattr(processor, method)(
+                    repository.get_complex_data()
+                ),
+            }
+        )
+    print(processed_data)
+    return processed_data
+
+
+def validate_data(processed_data: list):
+    for processed in processed_data:
+
+        method = str(processed["method"]).capitalize().replace("_", " ")
+        logger.info(
+            "-----------------------------------------\U0001f3af "
+            + method
+            + " results:\U0001f3af-----------------------------------------"
+        )
+        accuracy_percentage, list_difference = validator.validate_data(
+            processed["processed_data"],
+            repository.get_expected_complex_data(),
+        )
+        logger.info("\033[1m Well processed streets are:\033[0m")
+        for item in list_difference:
+            logger.info("\t" + str(item))
+        logger.info(
+            "\033[1m Percentage of accuracy of is: "
+            + str(round(accuracy_percentage))
+            + "%  \u2705 \033[0m"
+        )
+
+
+if __name__ == "__main__":
+    logger.info("Service started with version {}".format(__version__))
+
+    processor = init_processor()
+    repository = init_repository()
+    validator = init_validator()
+
+    logger.info(
+        "----------------------------------------Expected results-----------------------------------------"
+    )
+    expected_data = json.loads(repository.get_expected_complex_data())
+    for item in expected_data:
+        logger.info("\t" + str(item))
 
     try:
-        logger.info(
-            "Trying to split the following concatenated streets and numbers using:"
-            + str(Processor.simple_processor.__name__)
+
+        public_method_names = get_public_methods_from_processor(processor)
+        processed_data = call_all_processors(
+            public_method_names, processor, repository
         )
-        logger.info(
-            "----------------------------------------Expected results-----------------------------------------"
-        )
-        expected_data = json.loads(repository.get_expected_complex_data())
-        for item in expected_data:
-            logger.info("\t" + str(item))
 
-        public_method_names = [
-            method
-            for method in dir(processor)
-            if callable(getattr(processor, method))
-            if not method.startswith("_")
-        ]
-
-        for method in public_method_names:
-            processed_data.append(
-                {
-                    "method": method,
-                    "processed_data": getattr(processor, method)(
-                        repository.get_complex_data()
-                    ),
-                }
-            )
-
-        for processed in processed_data:
-
-            method = str(processed["method"]).capitalize().replace("_", " ")
-            logger.info(
-                "-----------------------------------------\U0001f3af "
-                + method
-                + " results:\U0001f3af-----------------------------------------"
-            )
-            accuracy_percentage, list_difference = validator.validate_data(
-                processed["processed_data"],
-                repository.get_expected_complex_data(),
-            )
-            logger.info("\033[1m Well processed streets are:\033[0m")
-            for item in list_difference:
-                logger.info("\t" + str(item))
-            logger.info(
-                "\033[1m Percentage of accuracy of is: "
-                + str(round(accuracy_percentage))
-                + "%  \u2705 \033[0m"
-            )
+        validate_data(processed_data)
 
     except Exception as e:
         logger.error("\u274C Error validating data, " + str(e) + "\u274C")
--- app/services/tests/test.py
+++ app/services/tests/test.py
@@ -3,9 +3,19 @@ import unittest
 
 import pytest
 
+from app.main import call_all_processors as call_all_processors_main
+from app.main import (
+    get_public_methods_from_processor as get_public_methods_from_processor_main,
+)
+from app.main import init_processor as init_processor_main
+from app.main import init_repository as init_repository_main
+from app.main import init_validator as init_validator_main
 from app.services.processor import Processor
+from app.services.repository import Repository
+from app.services.validator import Validator
 
 case = unittest.TestCase()
+case_set_up = unittest.TestCase().setUp()
 case.maxDiff = None
 
 
@@ -13,6 +23,18 @@ case.maxDiff = None
 def init_processor() -> Processor:
     processor = Processor()
     return processor
+
+
+@pytest.fixture
+def init_repository() -> Repository:
+    repository = Repository()
+    return repository
+
+
+@pytest.fixture
+def init_validator() -> Validator:
+    validator = Validator()
+    return validator
 
 
 @pytest.fixture
@@ -47,6 +69,69 @@ def complex_street_data_sample() -> list:
         "Calle Aduana, 29",
         "Calle 39 No 1540",
     ]
+
+
+@pytest.fixture
+def expected_simple_data_sample() -> list:
+
+    return json.dumps(
+        [
+            {"street": "Winterallee", "housenumber": "3"},
+            {"street": "Musterstrasse", "housenumber": "45"},
+            {"street": "Blaufeldweg", "housenumber": "123B"},
+        ],
+        ensure_ascii=False,
+    )
+
+
+@pytest.fixture
+def expected_middle_data_sample() -> list:
+
+    return json.dumps(
+        [
+            {"street": "Winterallee", "housenumber": "3"},
+            {"street": "Musterstrasse", "housenumber": "45"},
+            {"street": "Blaufeldweg", "housenumber": "123B"},
+            {"street": "Am Bächle", "housenumber": "23"},
+            {"street": "Auf der Vogelwiese", "housenumber": "23 b"},
+        ],
+        ensure_ascii=False,
+    )
+
+
+@pytest.fixture
+def expected_complex_data_sample() -> str:
+
+    return json.dumps(
+        [
+            {"street": "Winterallee", "housenumber": "3"},
+            {"street": "Musterstrasse", "housenumber": "45"},
+            {"street": "Blaufeldweg", "housenumber": "123B"},
+            {"street": "Am Bächle", "housenumber": "23"},
+            {"street": "Auf der Vogelwiese", "housenumber": "23 b"},
+            {"street": "rue de la revolution", "housenumber": "4"},
+            {"street": "Broadway Av", "housenumber": "200"},
+            {"street": "Calle Aduana", "housenumber": "29"},
+            {"street": "Calle 39", "housenumber": "No 1540"},
+        ],
+        ensure_ascii=False,
+    )
+
+
+@pytest.fixture
+def validation_data(expected_complex_data_sample) -> tuple[float, list]:
+    perfect_accuracy = 100.0
+    return perfect_accuracy, expected_complex_data_sample
+
+
+@pytest.fixture
+def get_public_methods_from_processor() ->list:
+    return ['complex_deepparse_processor', 'complex_processor', 'middle_processor', 'simple_processor']
+
+
+@pytest.fixture
+def call_all_processors() ->list:
+    return [{'method': 'complex_deepparse_processor', 'processed_data': '[{"street": "Winterallee", "housenumber": "3"}, {"street": "Musterstrasse", "housenumber": "45"}, {"street": "Blaufeldweg", "housenumber": "123b"}, {"street": "Am bächle", "housenumber": "23"}, {"street": "Auf der vogelwiese", "housenumber": "23 b"}, {"street": "Rue de la", "housenumber": "4"}, {"street": "Broadway av", "housenumber": "200"}, {"street": "Calle aduana", "housenumber": "29"}, {"street": "Calle", "housenumber": "39"}]'}, {'method': 'complex_processor', 'processed_data': '[{"street": "Winterallee", "housenumber": "3"}, {"street": "Musterstrasse", "housenumber": "45"}, {"street": "Blaufeldweg", "housenumber": "123B"}, {"street": "Am Bächle", "housenumber": "23"}, {"street": "Auf der Vogelwiese", "housenumber": "23b"}, {"street": "rue de la revolution", "housenumber": "4"}, {"street": "Broadway Av", "housenumber": "200"}, {"street": "Calle Aduana", "housenumber": "29"}, {"street": "39 No 1540", "housenumber": "Calle "}]'}, {'method': 'middle_processor', 'processed_data': '[{"street": "Winterallee", "housenumber": "3"}, {"street": "Musterstrasse", "housenumber": "45"}, {"street": "Blaufeldweg", "housenumber": "123B"}, {"street": "Am Bächle", "housenumber": "23"}, {"street": "Auf der Vogelwiese", "housenumber": "23 b"}, {"street": "", "housenumber": ""}, {"street": "", "housenumber": ""}, {"street": "Calle Aduana", "housenumber": ", 29"}, {"street": "Calle", "housenumber": "39 No 1540"}]'}, {'method': 'simple_processor', 'processed_data': '[{"street": "Winterallee", "housenumber": "3"}, {"street": "Musterstrasse", "housenumber": "45"}, {"street": "Blaufeldweg", "housenumber": "123B"}, {"street": "", "housenumber": ""}]'}]
 
 
 def test_simple_processor(init_processor, simple_street_data_sample):
@@ -132,3 +217,52 @@ def test_complex_deeparse_processor(init_processor, complex_street_data_sample):
         )
     )
     case.assertCountEqual(actual, expected)
+
+
+def test_get_complex_data(init_repository, complex_street_data_sample):
+    actual = Repository.get_complex_data(init_repository)
+    expected = complex_street_data_sample
+    case.assertCountEqual(actual, expected)
+
+
+def test_get_expected_complex_data(
+    init_repository, expected_complex_data_sample
+):
+    actual = json.loads(Repository.get_expected_complex_data(init_repository))
+    expected = json.loads(expected_complex_data_sample)
+    case.assertCountEqual(actual, expected)
+
+
+def test_validate_data(
+    init_validator,
+    expected_complex_data_sample,
+    validation_data,
+):
+    actual = expected_complex_data_sample
+    expected = expected_complex_data_sample
+    actual_accuracy, actual_matching_list = Validator.validate_data(
+        init_validator, actual, expected
+    )
+    expected_accuracy, expected_matching_list = validation_data
+    expected_matching_list = json.loads(expected_matching_list)
+    case.assertCountEqual(actual_matching_list, expected_matching_list)
+    case.assertEqual(actual_accuracy, expected_accuracy)
+
+
+def test_init_processor():
+    case.assertIsNotNone(init_processor_main())
+
+
+def test_init_validator():
+    case.assertIsNotNone(init_validator_main())
+
+
+def test_init_repository():
+    case.assertIsNotNone(init_repository_main())
+
+
+def test_get_public_methods_from_processor(get_public_methods_from_processor)_
+    case.assertEqual(get_public_methods_from_processor, get_public_methods_from_processor_main())
+
+def test_call_all_processors(call_all_processors)_
+    case.assertEqual(call_all_processors, call_all_processors_main())
